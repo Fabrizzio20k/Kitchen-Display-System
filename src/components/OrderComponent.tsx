a3bca1dc340673932392b7a4e3c8763bb4ab1282
Fabrizzio20k <fabrizzio785@gmail.com>
2024-05-22 15:06:22 -0500

    Fix styles

--- src/components/OrderComponent.tsx
+++ src/components/OrderComponent.tsx
@@ -10,10 +10,6 @@ export default function OrderComponent({ order }: { order: Order }) {
     const [showAll, setShowAll] = useState(false);
     const dispatch = useAppDispatch();
 
-    const handleToggleShowAll = () => {
-        setShowAll(!showAll);
-    };
-
     const getOrderBorderClass = (status: string) => {
         switch (status) {
             case "done":
@@ -40,6 +36,19 @@ export default function OrderComponent({ order }: { order: Order }) {
         }
     }
 
+    const getTextColor = (status: string) => {
+        switch (status) {
+            case "done":
+                return 'text-green-700';
+            case "preparing":
+                return 'text-yellow-700';
+            case "pending":
+                return 'text-red-700';
+            default:
+                return '';
+        }
+    }
+
     const handleCurrentState = (id: string, status: string) => {
         switch (status) {
             case "done":
@@ -57,18 +66,18 @@ export default function OrderComponent({ order }: { order: Order }) {
         <div className={`flex flex-col ${getOrderBorderClass(order.status)} border-2 h-fit max-w-96 rounded-xl overflow-hidden`}>
             <div className="flex flex-col">
                 <div className={`flex flex-row justify-between p-5 ${getOrderBackgroundColor(order.status)}`}>
-                    <h3 className="text-md font-semibold test">Customer: {order.customerName}</h3>
-                    <h3 className="text-md font-semibold test">{order.time}</h3>
-                    <h3 className="text-md font-semibold test">Status: {order.status}</h3>
+                    <h3 className="text-md font-semibold">To: {order.customerName}</h3>
+                    <h3 className="text-md font-semibold">{order.time}</h3>
+                    <h3 className={`text-md font-semibold ${getTextColor(order.status)}`}>{order.status}</h3>
                 </div>
-                <div className="flex flex-row justify-center space-x-4 py-4 bg-white">
-                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded" onClick={() => handleCurrentState(order.id, "done")}>
-                        Mark as delivered
+                <div className="flex flex-col items-center md:flex-row justify-center space-x-2 py-4 bg-white text-sm">
+                    <button className="w-fit bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded" onClick={() => handleCurrentState(order.id, "done")}>
+                        Delivered
                     </button>
-                    <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded" onClick={() => handleCurrentState(order.id, "preparing")}>
+                    <button className="w-fit bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded" onClick={() => handleCurrentState(order.id, "preparing")}>
                         Preparing
                     </button>
-                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" onClick={() => handleCurrentState(order.id, "pending")}>
+                    <button className="w-fit bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded" onClick={() => handleCurrentState(order.id, "pending")}>
                         Cancel
                     </button>
                 </div>
